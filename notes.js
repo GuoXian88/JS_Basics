75523d6a80d5a96b212d5018ffaf40ea40735d04
xianguo <xianguo@Ctrip.com>
2018-01-31 11:45:26 +0800

    add notes

--- notes.js
+++ notes.js
@@ -1,3 +1,6 @@
+
+//1.class static props
+
 class A { static getname() { return 'A'}}
 
 class B extends A { }
@@ -7,11 +10,97 @@ b = new B()
 B.getname() //'A'
 b.getname() // not work
 
-// fetch api
+/* 4.fetch api
+
+这种功能以前是使用  XMLHttpRequest实现的。Fetch提供了一个更好的替代方法，可以很容易地被其他技术使用，例如 Service Workers。Fetch还提供了单个逻辑位置来定义其他HTTP相关概念，例如 CORS和HTTP的扩展。
+
+请注意，fetch 规范与 jQuery.ajax() 主要有两种方式的不同，牢记：
+
+当接收到一个代表错误的 HTTP 状态码时，从 fetch()返回的 Promise 不会被标记为 reject， 即使该 HTTP 响应的状态码是 404 或 500。相反，它会将 Promise 状态标记为 resolve （但是会将 resolve 的返回值的 ok 属性设置为 false ），  仅当网络故障时或请求被阻止时，才会标记为 reject。
+默认情况下, fetch 不会从服务端发送或接收任何 cookies, 如果站点依赖于用户 session，则会导致未经认证的请求（要发送 cookies，必须设置 credentials 选项）.
+
+*/
+
 // web api service worker
 // nodejs koa express. middleware? 查看进程
+function emptyMiddleware (req, res, next) {
+    req.somedata = 42
+    next()
+}
+
 // 监控js性能，报错
 // 共享组件
 // React Portal. Popover. Modal Dialog.
 // 通用的逻辑封装
-window.onerror
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if (string.indexOf(substring) > -1){
+        alert('Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join(' - ');
+
+        alert(message);
+    }
+
+    return false;
+};
+
+
+
+/* 2.HOC 复用逻辑
+You can imagine that in a large app, this same pattern of subscribing to DataSource and calling setState will occur over and over again. We want an abstraction that allows us to define this logic in a single place and share them across many components. This is where higher-order components excel.
+
+Note that a HOC doesn’t modify the input component, nor does it use inheritance to copy its behavior. Rather, a HOC composes the original component by wrapping it in a container component. A HOC is a pure function with zero side-effects.
+
+When you apply a HOC to a component, though, the original component is wrapped with a container component. That means the new component does not have any of the static methods of the original component.
+
+*/
+const CommentListWithSubscription = withSubscription(
+  CommentList,
+  (DataSource) => DataSource.getComments()
+);
+
+const BlogPostWithSubscription = withSubscription(
+  BlogPost,
+  (DataSource, props) => DataSource.getBlogPost(props.id)
+);
+
+function logProps(WrappedComponent) {
+    return class extends React.Component {
+      componentWillReceiveProps(nextProps) {
+        console.log('Current props: ', this.props);
+        console.log('Next props: ', nextProps);
+      }
+      render() {
+        // Wraps the input component in a container, without mutating it. Good!
+        return <WrappedComponent {...this.props} />;
+      }
+    }
+  }
+
+  /*However, this requires you to know exactly which methods need to be copied. You can use hoist-non-react-statics to automatically copy all non-React static methods:
+  */
+  import hoistNonReactStatic from 'hoist-non-react-statics';
+  function enhance(WrappedComponent) {
+    class Enhance extends React.Component {/*...*/}
+    hoistNonReactStatic(Enhance, WrappedComponent);
+    return Enhance;
+  }
+
+// 3.React Portal
+  render() {
+    // React does *not* create a new div. It renders the children into `domNode`.
+    // `domNode` is any valid DOM node, regardless of its location in the DOM.
+    return ReactDOM.createPortal(
+      this.props.children,
+      domNode,
+    );
+  }
+  /*A typical use case for portals is when a parent component has an overflow: hidden or z-index style, but you need the child to visually “break out” of its container. For example, dialogs, hovercards, and tooltips.
