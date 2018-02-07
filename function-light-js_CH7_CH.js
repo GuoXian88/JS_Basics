a294aa1f6c71755ab244a08287b5f6cecd40c6e6
BeaverGuo <lalabeaver21@gmail.com>
2018-02-07 19:28:06 +0800

    fn-light

--- function-light-js_CH7_CH.js
+++ function-light-js_CH7_CH.js
@@ -29,3 +29,93 @@ var student = bind( StudentRecord, { name: "Kyle.." } );
 //However, the built-in bind(..) utility doesn't really have to create a closure to accomplish the task. It simply creates a function and manually sets its internal this to the specified object. That's potentially a more efficient operation than if we did the closure ourselves.
 
 //The kind of performance savings we're talking about here is miniscule on an individual operation. But if your library's critical path is doing this hundreds or thousands of times or more, that savings can add up quickly. Many libraries -- Bluebird being one such example -- have ended up optimizing by removing closures and going with objects, in exactly this means.
+
+
+// CH 8 recursive
+//there are more complex use cases where mutual recursion can be very helpful. Counting the number of leaves in a tree data structure is one example, and recursive descent parsing (of source code, by a compiler) is another.
+
+function sum(total,...nums) {
+    for (let num of nums) {
+        total = total + num;
+    }
+
+    return total;
+}
+
+// vs
+
+function sum(num1,...nums) {
+    if (nums.length == 0) return num1;
+    return num1 + sum( ...nums );
+}
+
+
+
+function maxEven(...nums) {
+    var maxNum = -Infinity;
+
+    for (let num of nums) {
+        if (num % 2 == 0 && num > maxNum) {
+            maxNum = num;
+        }
+    }
+
+    if (maxNum !== -Infinity) {
+        return maxNum;
+    }
+}
+
+function maxEven(num1,...restNums) {
+    var maxRest = restNums.length > 0 ?
+            maxEven( ...restNums ) :
+            undefined;
+
+    return (num1 % 2 != 0 || num1 < maxRest) ?
+        maxRest :
+        num1;
+}
+
+//But I'd say the most obvious improvement is that the distraction of the imperative for-loop is suppressed. All the looping logic is abstracted into the recursive call stack, so that stuff doesn't clutter the code. We're free then to focus on the logic of finding a max-even by comparing two numbers at a time -- the important part anyway!
+
+function depth(node) {
+    if (node) {
+        let depthLeft = depth( node.left );
+        let depthRight = depth( node.right );
+        return 1 + max( depthLeft, depthRight );
+    }
+
+    return 0;
+}
+
+//函数内部调用函数会形成stack
+//When the second function call starts, it needs a stack frame as well, bringing the count to 2. If that function calls another, we need a third stack frame. And so on. The word "stack" speaks to the notion that each time a function is called from the previous one, the next frame is stacked on top. When a function call finishes, its frame is popped off the stack.
+
+
+//The idea is that if a call from function baz() to function bar() happens at the very end of function baz()'s execution -- referred to as a tail call -- the stack frame for baz() isn't needed anymore. That means that either the memory can be reclaimed, or even better, simply reused to handle function bar()'s execution. Visualizing:
+
+//The main problem with recursion is its memory usage, keeping around the stack frames to track the state of a function call while it dispatches to the next recursive call iteration.
+
+//The key recognition point for this refactoring strategy is that we could remove our dependence on the stack by doing the addition now instead of after, and then forward-passing that partial result as an argument to the recursive call. In other words, instead of keeping total in the current function's stack frame, push it into the stack frame of the next recursive call; that frees up the current stack frame to be removed/reused.
+
+"use strict";
+
+function sum(result,num1,...nums) {
+    result = result + num1;
+    if (nums.length == 0) return result;
+    return sum( result, ...nums );
+}
+
+
+"use strict";
+
+function maxEven(num1,num2,...nums) {
+    num1 =
+        (num1 % 2 == 0 && !(maxEven( num2 ) > num1)) ?
+            num1 :
+            (num2 % 2 == 0 ? num2 : undefined);
+
+    return nums.length == 0 ?
+        num1 :
+        maxEven( num1, ...nums )
+}
+
