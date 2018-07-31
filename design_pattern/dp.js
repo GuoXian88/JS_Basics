6312bfa531141052b7ecbd4e9aa96f6b4c9afcb2
guo <guoxian@evergrande.cn>
2018-07-31 15:10:44 +0800

    add mastering nodejs

---
+++ design_pattern/dp.js
@@ -0,0 +1,17 @@
+/*
+Filter pattern or Criteria pattern 
+filter a set of objects using different criteria and chaining them in a decoupled way through logical operations. This type of design pattern comes under structural pattern as this pattern combines multiple criteria to obtain single criteria.
+
+Filter design pattern is useful where you want to add filters dynamically or you are implementing multiple functionalities and most of them require different filter criteria to filter something. In that case instead of hard coding the filters inside the functionalities, you can create filter criteria and re-use it wherever required.
+
+List<Laptop> laptops = LaptopFactory.manufactureInBulk();
+AndCriteria searchCriteria = new AndCriteria(
+  new HardDisk250GBFilter(), 
+  new MacintoshFilter(), 
+  new I5ProcessorFilter());
+List<Laptop> filteredLaptops = searchCriteria.meets(laptops);
+
+
+In Strategy pattern, a class behavior or its algorithm can be changed at run time. This type of design pattern comes under behavior pattern.
+
+In Strategy pattern, we create objects which represent various strategies and a context object whose behavior varies as per its strategy object. The strategy object changes the executing algorithm of the context object.
